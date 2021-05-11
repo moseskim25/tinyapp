398ca1d2d20b0f9dbcc840b76561a756d525c2b6
Moses Kim <moseskim4740@gmail.com>
2021-05-11 19:52:19 +0000

    Added POST route to track username using cookies

--- express_server.js
+++ express_server.js
@@ -74,6 +74,11 @@ app.post('/urls/:shortURL', (req, res) => {
   res.redirect('/urls');
 })
 
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
