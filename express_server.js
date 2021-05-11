cb2b5e66df1eccbe378770f20879f976b4d3f7ca
Moses Kim <moseskim4740@gmail.com>
2021-05-11 16:25:31 +0000

    Delete button

--- express_server.js
+++ express_server.js
@@ -60,6 +60,13 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//DELETES URL
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const {shortURL} = req.params;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
